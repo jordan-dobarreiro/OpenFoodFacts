66079fe6d2f7601af1c6c6db785529137143afbb
jordan-dobarreiro <jordan.dobarreiro@gmail.com>
2020-03-30 00:07:25 +0200

    a

--- BOUTAKHOT-DO BARREIRO-LANCMAN/MainWindow.xaml.cs
+++ BOUTAKHOT-DO BARREIRO-LANCMAN/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace BOUTAKHOT_DO_BARREIRO_LANCMAN
 {
@@ -20,40 +22,86 @@ namespace BOUTAKHOT_DO_BARREIRO_LANCMAN
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> m_list_categories;
-
+        public List<string> m_list_categories;
+        public List<Product> m_list_products;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //public void GetAllProducts(string url)
+        public void GetAllProducts()
+        {
+            string url = "https://fr.openfoodfacts.org/categorie/pains.json";
+            WebClient webClient = new WebClient();
+            var json = webClient.DownloadString(url);
+            JToken jtokens = JObject.Parse(json);
+            //list<JToken> jtokens = JObject.Parse(json).Children().ToList();
+            foreach (JToken jtoken in jtokens)
+            {
+                Product product = new Product
+                {
+                    product_name = (string)jtoken["product_name"],
+                    image_url = (string)jtoken["image_thumb_url"],
+                    quantity = (string)jtoken["quantity"],
+                    expiration_date = (string)jtoken["expiration_date"],
+                    brand = (string)jtoken["brands"],
+                    nutriscore = (string)jtoken["nutriscore_grade"],
+                    ingredients = (string)jtoken["ingredients_text"],
+                    barcode = (string)jtoken["id"]
+                };
+                if (!String.IsNullOrEmpty(product.product_name))
+                {
+                    m_list_products.Add(product);
+                }
+            }
+        }
+
+        /*public void PrintAllProducts()
+        {
+            foreach (Product prod in m_list_products)
+            {
+                Console.WriteLine("oui");
+                Console.WriteLine(prod);
+            }
+        }*/
+
         private void SearchNameClick(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("bite");
         }
 
         private void SearchBarecodeClick(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("bite");
         }
 
         private void SearchCategoryClick(object sender, RoutedEventArgs e)
         {
-
+            GetAllProducts();
         }
 
         private void CategorySelectChange(object sender, SelectionChangedEventArgs e)
         {
-
+            MessageBox.Show("bite");
         }
 
         public List<string> ListCategories
         {
             get { return m_list_categories; }
-            set 
+            set
             {
                 m_list_categories = value;
-               /// OnPropertyChanged("ListCategories");
+                /// OnPropertyChanged("ListCategories");
+            }
+        }
+        public List<Product> ListProducts
+        {
+            get { return m_list_products; }
+            set
+            {
+                m_list_products = value;
+                /// OnPropertyChanged("ListProducts");
             }
         }
     }
---
+++ BOUTAKHOT-DO BARREIRO-LANCMAN/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOUTAKHOT_DO_BARREIRO_LANCMAN
+{
+    public class Product
+    {
+        public string product_name { get; set; }
+        public string image_url { get; set; }
+        public string quantity { get; set; }
+        public string expiration_date { get; set; }
+        public string brand { get; set; }
+        public string nutriscore { get; set; }
+        public string ingredients { get; set; }
+        public string barcode { get; set; }
+    }
+}
